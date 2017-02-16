8f1dfbd83ea68f6f5f0dbd9c51568fe6b7a92b41
iijijii <lilysatoo@gmail.com>
2017-02-16 15:26:39 +0900

    Implement LinkedList.insert

--- linked-list.js
+++ linked-list.js
@@ -26,7 +26,34 @@ function LinkedList(){
 		length++;
 	};
 
-	this.insert = function(position, element){};
+	this.insert = function(position, element){
+		var node = new Node(element),
+			current = head,
+			previous
+			index = 0;
+
+		if (position > length - 1 || position < 0){
+			return false;
+		}
+
+		if (position === 0){
+			node.next = current;
+			head = node;
+		}
+		else{
+			while(index++ < position){
+				previous = current;
+				current = current.next;
+			}
+			node.next = current;
+			previous.next = node;
+		}
+
+		length++;
+
+		return true;
+	};
+
 	this.removeAt = function(position){};
 	this.remove = function(element){};
 	this.indexOf = function(element){};
