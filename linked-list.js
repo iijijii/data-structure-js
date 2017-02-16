52c5b5d66394fa9f034514b6807684629d2a86cc
iijijii <lilysatoo@gmail.com>
2017-02-16 14:54:30 +0900

    Implement LinkedList.append

--- linked-list.js
+++ linked-list.js
@@ -8,7 +8,24 @@ function LinkedList(){
 	var length = 0;
 	var head = null;
 
-	this.append = function(element){};
+	this.append = function(element){
+		var node = new Node(element),
+			current;
+
+		if (head == null){
+			head = node;
+		}
+		else{
+			current = head;
+			while(current.next){
+				current = current.next;
+			}
+			current.next = node;
+		}
+
+		length++;
+	};
+
 	this.insert = function(position, element){};
 	this.removeAt = function(position){};
 	this.remove = function(element){};
