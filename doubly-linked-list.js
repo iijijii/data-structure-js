bcc36b904d358f4434d9813538b2bbb27f75a46b
iijijii <lilysatoo@gmail.com>
2017-02-17 10:48:28 +0900

    Design DoublyLinkedList

---
+++ doubly-linked-list.js
@@ -0,0 +1,15 @@
+function DoublyLinkedList(){
+	var Node = function(element){
+		this.element = element;
+		this.next = null;
+		this.prev = null;
+	}
+
+	var length = 0;
+	var head = null;
+	var tail = null;
+
+	var insert = function(position, element){};
+
+	var removeAt= function(position){};
+}
