609e05913ad2e76fb5aadf5567c3e2a646978791
iijijii <lilysatoo@gmail.com>
2017-02-17 10:49:25 +0900

    Implement DoubleLinkedList.insert

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -9,7 +9,49 @@ function DoublyLinkedList(){
 	var head = null;
 	var tail = null;
 
-	var insert = function(position, element){};
+	var insert = function(position, element){
+		var node = new Node(element),
+			current = head,
+			previous,
+			index = 0;
+
+		if (position > length || position < 0){
+			return false;
+		}
+
+		if (position === 0){
+
+			if (head){
+				node.next = current;
+				current.prev = node;
+				head = node;
+			}else{
+				head = node;
+				tail = node;
+			}
+
+		}else if (position === length){
+			current = tail;
+			current.next = node;
+			node.prev = current;
+			tail = node;
+		}
+		else{
+
+			while(index++ < position){
+				previous = current;
+				current = current.next;
+			}
+			node.next = current;
+			current.prev = node;
+			previous.next = node;
+			node.prec = previous;
+		}
+
+		length++;
+
+		return true;
+	};
 
 	var removeAt= function(position){};
 }
