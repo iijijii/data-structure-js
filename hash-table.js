2314b6d0c4b0ab8daa536b4bcc4155ea1ad357f5
iijijii <lilysatoo@gmail.com>
2017-02-20 12:07:47 +0900

    Add loseloseHashCode

--- hash-table.js
+++ hash-table.js
@@ -1,6 +1,15 @@
 function HashTable(){
 	var table = [];
 
+	var loseloseHashCode = function (key)
+	{
+		var hash = 0;
+		for (var i=0; i< key.length; i++){
+			hash += key.charCodeAt(i);
+		}
+		return hash % 37;
+	}; 
+
 	this.put = function(key, value){};
 	this.remove  = function(key){};
 	this.get = function(key){};
