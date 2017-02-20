bd6c73c7439bd32a32aabb4c8e835a985265856f
iijijii <lilysatoo@gmail.com>
2017-02-20 11:17:28 +0900

    Design dictionary

--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,12 @@
 function Dictionary(){
 	var items = {};
+
+	this.set = function(key, value){};
+	this.remove = function(key){};
+	this.has = function(key){};
+	this.get = function(key){};
+	this.clear = function(){};
+	this.size = function(){};
+	this.keys = function(){};
+	this.values = function(){};
 }
