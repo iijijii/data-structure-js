48740bfb77d79356e73fbf30576cabc082dc3d2c
iijijii <lilysatoo@gmail.com>
2017-02-21 14:18:10 +0900

    Add hash-table-linear-probing.js as copy of hash-table.js

---
+++ hash-table-linear-probing.js
@@ -0,0 +1,28 @@
+function HashTable(){
+	var table = [];
+
+	var loseloseHashCode = function (key)
+	{
+		var hash = 0;
+		for (var i=0; i< key.length; i++){
+			hash += key.charCodeAt(i);
+		}
+		return hash % 37;
+	};
+
+	this.put = function(key, value){
+		var position = loseloseHashCode(key);
+		console.log(position + ' - ' + key);
+		table[position] = value;
+	};
+
+	this.remove  = function(key){
+		var position = loseloseHashCode(key);
+		table[position] = undefined;
+	};
+
+	this.get = function(key){
+		var position = loseloseHashCode(key);
+		return table[position];
+	};
+}
