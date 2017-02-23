7748a826ea937481a901774eb245e13f3af9d1a8
iijijii <lilysatoo@gmail.com>
2017-02-23 15:46:53 +0900

    Add mergeSort

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -56,4 +56,44 @@ function ArrayList(){
 			array[j] = temp;
 		}
 	};
+
+	this.mergeSort = function(){
+		array = mergeSortRec(array);
+	};
+
+	var mergeSortRec = function(array){
+		var length = array.length;
+		if (length === 1){
+			return array;
+		}
+		var mid = Math.floor(length / 2);
+		var left = array.slice(0, mid);
+		var right = array.slice(mid, length);
+
+		return merge(mergeSortRec(left), mergeSortRec(right));
+	};
+
+	var merge = function(left, right){
+		var result = [];
+		var il = 0;
+		var ir = 0;
+
+		while (il < left.length && ir < right.length){
+			if (left[il] < right[ir]){
+				result.push(left[il++]);
+			} else{
+				result.push(right[ir++]);
+			}
+		}
+
+		while (il < left.length){
+			result.push(left[il++]);
+		}
+
+		while (ir < right.length){
+			result.push(right[ir++]);
+		}
+
+		return result;
+	};
 }
