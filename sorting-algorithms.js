ce5a93b6ee5c7d7f518ad94c6ff14a8a1c67df55
iijijii <lilysatoo@gmail.com>
2017-02-23 14:48:57 +0900

    Add ArrayList.bubbleSort

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -11,4 +11,19 @@ function ArrayList(){
 	this.toString = function(){
 		return array.join();
 	};
+
+	var swap = function(array, index1, index2){
+		[array[index1], array[index2]] = [array[index2], array[index1]];
+	};
+
+	this.bubbleSort = function(){
+		var length = array.length;
+		for (var i = 0; i < length; i++){
+			for (var j = 0; i<length - 1; j++){
+				if (array[j] > array[j + 1]){
+					swap(array, j, j + 1);
+				}
+			}
+		}
+	};
 }
