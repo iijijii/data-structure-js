8b127dd84a210a6a9ed93e1da6375d797defc4d0
iijijii <lilysatoo@gmail.com>
2017-02-23 14:41:10 +0900

    Add ArrayList

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -1,2 +1,14 @@
 function SortingAlgorithms{
 }
+
+function ArrayList(){
+	var array = [];
+
+	this.insert = function(item){
+		array.push(item);
+	};
+
+	this.toString = function(){
+		return array.join();
+	};
+}
