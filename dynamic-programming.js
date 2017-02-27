a3a489af6fab69f0f29ef0e8fd8c53ed06e26eb7
iijijii <lilysatoo@gmail.com>
2017-02-27 12:25:27 +0900

    Add dynamic programming MinCoinChange

---
+++ dynamic-programming.js
@@ -0,0 +1,34 @@
+function MinCoinChange(coins){
+	var coins = coins;
+	var cache = {};
+
+	this.makeChange = function(amount){
+		var me = this;
+		if (!amount){
+			return [];
+		}	
+		if (cache[amount]){
+			return cache[amount];
+		}
+
+		var min = [];
+		var newMin;
+		var newAmount;
+
+		for (var i = 0; i < coins.length; i++){
+			var coin = coins[i];
+			newAmount = amount - coin;
+			if (newAmount >= 0){
+				newMin = me.makeChange(newAmount);
+			}
+			if (newAmount >= 0
+				&& (newMin.length < min.length - 1 || !min.length)
+				&& (newMin.length || !newAmount)
+			){
+				min = [coin].concat(newMin);
+					console.log('new Min ' + min + ' for ' + amount);
+			}
+		}
+		return (cache[amount] = min);
+	};
+}
