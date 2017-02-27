ab44cacd2693996bf52c5c49b4a5376624ed8a92
iijijii <lilysatoo@gmail.com>
2017-02-27 11:50:34 +0900

    Add binarySearch

--- sorting-searching-algorithms.js
+++ sorting-searching-algorithms.js
@@ -173,4 +173,26 @@ function ArrayList(){
 			heapify(array, heapSize, largest);
 		}
 	};
+
+	var binarySearch = function(item){
+		this.quickSort();
+
+		var low = 0;
+		var high = array.length - 1;
+		var mid;
+		var element;
+
+		while(low <= high){
+			mid = Math.floor((low + high) / 2);
+			element = array[mid];
+			if (element < item){
+				low = mid + 1;
+			} else if (element > item){
+				high = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	};
 }
